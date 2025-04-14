aed2e1aa12c4125f76096c6cb374bc6648a7248d
Nicolas Andrew Burnett <nicolasandrewburnett@gmail.com>
2025-04-13 22:45:04 -0400

    feat(nvim): update telescope keybindings

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -2,5 +2,11 @@ return {
 	"nvim-telescope/telescope.nvim",
 	dependencies = {
 		"nvim-lua/plenary.nvim"
-	}
+	},
+	config = function()
+		local builtin = require("telescope.builtin")
+
+		vim.keymap.set("n", "<leader>p", builtin.find_files, { desc = "Find files" })
+		vim.keymap.set("n", "<leader>g", builtin.git_files, { desc = "Find files in Git repo" })
+	end
 }
