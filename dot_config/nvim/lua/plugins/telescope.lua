4f3e80be71aba25a57382fd147eddede9dd457fe
Nicolas Andrew Burnett <nicolasandrewburnett@gmail.com>
2025-04-13 23:41:18 -0400

    feat(nvim): better fzf

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -6,8 +6,27 @@ return {
 	config = function()
 		local builtin = require("telescope.builtin")
 
-		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
-		vim.keymap.set("n", "<leader>fG", builtin.git_files, { desc = "Find files globally" })
+		vim.keymap.set("n", "<leader>ff", function()
+			builtin.find_files({
+				prompt_title = "Directory Search",
+				find_command = {
+					"fd",
+					"--type", "f",
+					"--hidden"
+				}
+			})
+		end, { desc = "Find files" })
+		vim.keymap.set("n", "<leader>fG", function()
+			builtin.find_files({
+				prompt_title = "Global Search",
+				cwd = vim.fn.expand("~"),
+				find_command = {
+					"fd",
+					"--type", "f",
+					"--hidden",
+				}
+			})
+		end, { desc = "Find files globally" })
 		vim.keymap.set("n", "<leader>fg", builtin.git_files, { desc = "Find files in Git repo" })
 	end
 }
