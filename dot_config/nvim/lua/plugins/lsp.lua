5d6b460f0e2135d076d4715841c7a02db503f072
Nicolas Andrew Burnett <nicolasandrewburnett@gmail.com>
2025-05-06 12:32:51 -0400

    feat(nvim): fonts + minor diagnostic changes

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -1,13 +1,25 @@
 return {
-    "neovim/nvim-lspconfig",
-    name = "lsp",
-    lazy = false,
-    config = function()
-        require("lsp")
-        vim.fn.sign_define("DiagnosticSignWarn", { text = "", texthl = "DiagnosticSignWarn" })
-        vim.fn.sign_define("DiagnosticSignError", { text = "", texthl = "DiagnosticSignError" })
-        vim.fn.sign_define("DiagnosticSignHint",  { text = "󰌵", texthl = "DiagnosticSignHint" })
-        vim.fn.sign_define("DiagnosticSignInfo",  { text = "", texthl = "DiagnosticSignInfo" })
-    end
+        "neovim/nvim-lspconfig",
+        name = "lsp",
+        lazy = false,
+        config = function()
+                require("lsp")
+                vim.diagnostic.config({
+                        signs = {
+                                text = {
+                                        [vim.diagnostic.severity.ERROR] = "",
+                                        [vim.diagnostic.severity.WARN] = "",
+                                        [vim.diagnostic.severity.INFO] = "",
+                                        [vim.diagnostic.severity.HINT] = "󰌵",
+                                },
+                                linehl = {
+                                        [vim.diagnostic.severity.ERROR] = "Error",
+                                        [vim.diagnostic.severity.WARN] = "Warn",
+                                        [vim.diagnostic.severity.INFO] = "Info",
+                                        [vim.diagnostic.severity.HINT] = "Hint",
+                                }
+                        },
+                })
+        end
 }
 
