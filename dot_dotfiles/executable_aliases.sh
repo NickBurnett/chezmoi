cfa6da8447cda597e182812ac907d230742db52b
Nicolas Burnett <nicolasandrewburnett@gmail.com>
2026-04-01 23:16:26 -0400

    feat: add dotfiles and basic oh-my-zsh

---
+++ dot_dotfiles/executable_aliases.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# Core
+alias n="nvim"
+alias v="nvim"
+alias pm="pacman"
+alias chez="chezmoi"
+alias g="git"
+
+# Base
+## Mainly for configuring defaults for CLI tools.
+alias ls="ls -a --color=auto"
+alias grep="grep --color=auto"
+
+# Git
+alias gsw="git switch"
+alias pull="git pull"
+alias push="git push"
---
+++ dot_dotfiles/executable_omz.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+omz_dirpath="$HOME/.oh-my-zsh"
+[[ ! -d "$omz_dirpath" ]] && exit 0
+
+source "$omz_dirpath/oh-my-zsh.sh"
