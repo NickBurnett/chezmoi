5cbad281a385a9beb349901e1cd8e4e298a62e0c
Nicolas Burnett <nicolasandrewburnett@gmail.com>
2026-04-01 23:19:44 -0400

    feat: add basic omz configs

--- dot_dotfiles/executable_omz.sh
+++ dot_dotfiles/executable_omz.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env sh
 
+# Preflight
 omz_dirpath="$HOME/.oh-my-zsh"
 [[ ! -d "$omz_dirpath" ]] && exit 0
+
+# Basic Config
+ZSH_THEME="robbyrussel"
+plugins=(
+	git
+)
 
 source "$omz_dirpath/oh-my-zsh.sh"
